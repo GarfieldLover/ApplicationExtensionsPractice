743a9b2857db39f2655253a1b6b923aa799b5fca
张科 <zhangke19880503@163.com>
2016-07-19 21:42:32 +0800

    ApplicationExtensionsPractice

---
+++ ApplicationExtensionsPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApplicationExtensionsPractice
+//
+//  Created by zhangke on 16/7/19.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
