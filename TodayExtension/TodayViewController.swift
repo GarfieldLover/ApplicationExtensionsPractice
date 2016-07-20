79c3159bfb4fa5450b6c2b68b8e7a2725c0cff86
张科 <zhangke19880503@163.com>
2016-07-20 12:12:26 +0800

    today

--- TodayExtension/TodayViewController.swift
+++ TodayExtension/TodayViewController.swift
@@ -14,6 +14,8 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
+        
+        self.extensionContext?.widgetLargestAvailableDisplayMode = NCWidgetDisplayMode.compact
     }
     
     override func didReceiveMemoryWarning() {
@@ -23,12 +25,15 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     func widgetPerformUpdate(completionHandler: ((NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
-        
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
         
         completionHandler(NCUpdateResult.newData)
+    }
+    
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        
     }
     
 }
